4c852cfba072835aeec548ee82406ea6f64d7ddb
Julian Laffey <julianlaffey@gmail.com>
2015-02-13 14:58:47 +1100

    Store data in futures for other fns to access

--- src/transit/core.clj
+++ src/transit/core.clj
@@ -8,23 +8,25 @@
 
 
 (def ballarat-stops
-  (let [north	  -37.511713
-		west      143.779905
-		south     -37.638240
-		east      143.923586
-		griddepth 1
-		limit     600]
-	(ptv/fetch-points-of-interest ptv/BUS north west south east griddepth limit)
-	))
+  (future
+	  (let [north	  -37.511713
+			west      143.779905
+			south     -37.638240
+			east      143.923586
+			griddepth 1
+			limit     600]
+		(ptv/fetch-points-of-interest ptv/BUS north west south east griddepth limit)
+		)))
 
 
-(def routes (mapcat ptv/fetch-lines (map :stop_id ballarat-stops)))
+(def routes
+  (future (set (mapcat ptv/fetch-lines (map :stop_id @ballarat-stops)))))
 
 
 (defn make-stops-csv
   "Build Ballarat GTFS stops file from PTV data"
   []
-  (->> ballarat-stops
+  (->> @ballarat-stops
 	   (map ptv/stop-to-gtfs)
 	   (csv/to-csv ["stop_id" "stop_name" "stop_lat" "stop_lng"])
 	   ))
@@ -34,7 +36,7 @@
   "Build Ballarat GTFS routes file from static data"
   []
   (let [f #(vector (:line_id %) (:line_name %) "" gtfs/BUS)]
-	(->> (set routes)
+	(->> @routes
 		 (map f)
 		 (csv/to-csv ["route_id" "route_short_name" "route_long_name" "route_type"])
 		 )))
@@ -60,16 +62,18 @@
   (println "Building feed for Ballarat bus stops")
   (println "Creating" "feed/stops.txt")
   (spit "feed/stops.txt" (make-stops-csv))
-  ;(println "Creating" "feed/routes.txt")
-  ;(spit "feed/routes.txt" (make-routes-csv))
+  (println "Creating" "feed/routes.txt")
+  (spit "feed/routes.txt" (make-routes-csv))
   (println "Creating" "feed/calendar.txt")
   (spit "feed/calendar.txt" (make-calendar-csv))
   (println "Done")
   )
 
 
-(def headings ["service_id" "monday" "tuesday" "start_date"])
+(shutdown-agents) ; why?
 
-(clojure.string/join "," headings)
+;(ptv/fetch-line-run-ids (-> routes first :line_id))
+
+
 
 
